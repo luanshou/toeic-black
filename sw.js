eab6929bd5dfa61bfa8ffc898e6460200c465fda
luanshou <108936510+luanshou@users.noreply.github.com>
2026-07-05 17:59:07 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+var CACHE = "toeic-v1";
+var URLS = ["/", "/p22.html", "/manifest.json"];
+self.addEventListener("install", function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(URLS); }));
+  self.skipWaiting();
+});
+self.addEventListener("activate", function(e) {
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
+  }));
+  self.clients.claim();
+});
+self.addEventListener("fetch", function(e) {
+  e.respondWith(caches.match(e.request).then(function(cached) {
+    return cached || fetch(e.request).then(function(resp) {
+      if (resp.ok) {
+        var clone = resp.clone();
+        caches.open(CACHE).then(function(cache) { cache.put(e.request, clone); });
+      }
+      return resp;
+    });
+  }));
+});
